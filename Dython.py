5e8e11955067629f2510823ca77bc122495416c3
18001767679 <65285239+18001767679@users.noreply.github.com>
2020-06-30 20:36:20 +0800

    some minor changes

--- Dython.py
+++ Dython.py
@@ -1,5 +1,5 @@
 # Dython Moclude
-
+#You need a self param for every
 # Oprations
 class math():
     '''def expon(num, root): You can use math.pow(num,root) for this.
@@ -7,7 +7,7 @@ class math():
         for j in range(root - 1):
             ji = float(float(ji) * float(num))
         return ji'''
-    def itemsAdd(var):
+    def itemsAdd(s,var):
         num = 0
         for i in list(var):
             num += float(i)
@@ -17,7 +17,7 @@ class math():
         for i in list(var):
             num -= float(i) You should not combine non-associative functions.
         return num'''
-    def itemsMulti(var):
+    def itemsMulti(s,var):
         num = 1
         for i in list(var):
             num = float(float(num) * float(i))
@@ -27,7 +27,7 @@ class math():
         for i in range(int(len(list(var))) - 1):
             num = float(float(num) / float(var[i + 1]))
         return num'''
-    def joinStrs(strings):
+    def joinStrs(s,strings):
         string = ""
         for i in list(strings):
             string += str(i)
@@ -37,59 +37,64 @@ class math():
         for i in list(var): list.count is enough.
             if i is item: count += 1
         return int(count)'''
-    def rangeLimit(num, MIN, MAX):
+    def rangeLimit(s,num, MIN, MAX):
         if int(num) > int(MAX): return int(MAX)
         elif int(num) < int(MIN): return int(MIN)
         else: return int(num)
-    def feturn(con, true, false):#Inline if exp
+    def feturn(s,con, true, false):#Inline if exp
         return true if con else false
-    def listInit(var, item):
-        for j in range(len(list(var))):
+    '''def listInit(var, item):
+        for j in range(len(list(var))):list.index is the same
             if list(var)[j] is item: return int(j)
-        return None
-    def listInits(var, item):
+        return None'''
+    def listInits(s,var, item):
         items = []
         for j in range(len(list(var))):
             if list(var)[j] is item: items.append(j)
-        if items != []: return list(items)
+        if items != []: return list(items) 
         else: return None
-    def filterList(var, item):
+    def filterList(s,var, item):
         items = []
         for j in list(var):
             if not j is item: items.append(j)
         return items
+    def map(funct,dt):
+        for i in dt:
+            funct(dt)
         
 # Temp Variables
-temps = []
+#temps = []
 # [ [name, value] ]
 
 class temp():
-    def define(name, start):
+    def __init__(self):
+        self.temps=[]
+    def define(s,name, start):
         b = True
-        for i in list(temps):
+        for i in list(s.temps):
             if list(i)[0] is str(name):
                 b = False
                 break
-        if bool(b): temps.append([str(name), start])
-    def get(name):
-        for i in list(temps):
+        if bool(b): s.temps.append([str(name), start])
+    def get(s,name):
+        for i in list(s.temps):
             if list(i)[0] is str(name):
                 return list(i)[1]
         return None
-    def set(name, value):
+    def set(s,name, value):
         count = 0
-        for i in list(temps):
+        for i in s.temps:
             if list(i)[0] is str(name):
-                temps[int(count)] = [str(name), value]
+                s.temps[int(count)] = [str(name), value]
             count += 1
-    def remove(name):
-        for i in range(int(len(temps))):
+    def remove(s,name):
+        for i in range(int(len(s.temps))):
             j = list(temps)[i]
-            if list(j)[0] is str(name): temps.pop(i)
+            if list(j)[0] is str(name): s.temps.pop(i)
 
 # Console
 class console():
-    def menu(caption, options, noAnwser):
+    def menu(s,caption, options, noAnwser):
         print('')
         if caption != "" and caption != None: print(str(caption))
         if int(len(options)) <= 1:
